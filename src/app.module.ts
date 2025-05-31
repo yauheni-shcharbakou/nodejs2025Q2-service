5547c9da5adc1c51a45441b5212c64154168887a
Evgenii Shcherbakov <iipekolict@gmail.com>
2025-05-31 19:24:53 +0400

    refactor: replace config module logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './modules/config.module';
+import { ConfigModule } from './modules/config/config.module';
 
 @Module({
   imports: [ConfigModule],
